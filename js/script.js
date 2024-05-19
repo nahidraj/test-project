8c6dbe7aecd87f4bc1438cff177fa1ca69973bc4
Khairul Alam Nahid <123856849+nahidraj@users.noreply.github.com>
2024-05-19 21:10:55 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(function () {
+  "use strict";
+
+  // // Animation on scroll
+  // $(window).on("load", function () {
+  //   setTimeout(() => {
+  //     AOS.init({
+  //       once: false,
+  //     });
+  //   }, 0);
+  // });
+
+  // // counter up js
+  // $('.counter').counterUp({
+  //   delay: 50,
+  //   time: 2000
+  // });
+
+  //Fact Counter + Text Count
+  if ($(".count-box").length) {
+    $(".count-box").appear(
+      function () {
+        var $t = $(this),
+          n = $t.find(".count-text").attr("data-stop"),
+          r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+
+        if (!$t.hasClass("counted")) {
+          $t.addClass("counted");
+          $({
+            countNum: $t.find(".count-text").text(),
+          }).animate(
+            {
+              countNum: n,
+            },
+            {
+              duration: r,
+              easing: "linear",
+              step: function () {
+                $t.find(".count-text").text(Math.floor(this.countNum));
+              },
+              complete: function () {
+                $t.find(".count-text").text(this.countNum);
+              },
+            }
+          );
+        }
+      },
+      { accY: 0 }
+    );
+  }
+
+});
